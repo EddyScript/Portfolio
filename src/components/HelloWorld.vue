f10ff16bb22fdd0485f35332bb6b2f46608f0e60
EddyBalan <vic.balan.27@live.com>
2020-01-15 20:13:37 -0600

    2nd commit, adding vuetify

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,4 +1,3 @@
-// eslint-disable-next-line vue/valid-template-root
 <template>
 <div></div>
 </template>
@@ -14,18 +13,4 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
