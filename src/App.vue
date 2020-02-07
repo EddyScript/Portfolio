ee835affd3563f402d4e5a52a9f80169538945ee
EddyBalan <vic.balan.27@live.com>
2020-02-07 13:10:25 -0600

    5th commit trying to fix footer

--- src/App.vue
+++ src/App.vue
@@ -114,49 +114,9 @@
         </v-card>
          
       </div>
-          
-    <!-- footer -->
-        
-    <v-footer
-    dark
-    padless
-  >
-    <v-card
-      class="flex"
-      flat
-      tile
-    >
-      <v-card-title class="orange lighten-1" >
-          <div class="mx-auto">
-            <v-btn icon class="px-8">
-              <v-icon v-on:click= "twitter">mdi-twitter</v-icon>
-            </v-btn>
-
-        
-
-            <v-btn icon class="px-8">
-              <v-icon v-on:click= "github">mdi-github-circle</v-icon>
-            </v-btn>
-
-           
-
-            <v-btn icon class="px-8">
-              <v-icon v-on:click = "linkedin">mdi-linkedin-box</v-icon>
-            </v-btn>
-            
-          </div>
-
-          
-            
       
-      </v-card-title>
-
-      <v-card-text class="py-2 white--text text-center orange lighten-1">
-        {{ new Date().getFullYear() }} — <strong>Eddy's Portfolio</strong>
-      </v-card-text>
-    </v-card>
-  </v-footer>
-
+      <bottomNav></bottomNav>
+     
   </v-app>
 
   
@@ -164,28 +124,20 @@
 
 <script>
 
+import bottomNav from './components/bottomNav'
 
 export default {
   name: 'App',
 
   components: {
-    
+    bottomNav
   },
 
   data: () => ({
   }),
-  methods: {
-    twitter: function () {
-      window.location.href = "https://twitter.com/SandwichPirate_"
-    },
-    github: function() {
-      window.location.href = "https://github.com/EddyScript"
-    },
-    linkedin: function() {
-      window.location.href = "https://www.linkedin.com/in/eddy-balan-270132175/"
-    }
-  }
+  methods: {}
 };
+
 </script>
 
 <style scoped>
---
+++ src/components/bottomNav.vue
@@ -0,0 +1,73 @@
+<template>
+    <!-- footer -->
+        
+  <v-footer
+      padless
+      width="auto"
+    >
+      <v-card
+        class="flex"
+        flat
+        tile
+        width="auto"
+      >
+        <v-card-title 
+        class="orange lighten-1" 
+        >
+
+            <div class="mx-auto">
+
+              <v-btn icon class="px-8">
+                <v-icon v-on:click= "twitter">mdi-twitter</v-icon>
+              </v-btn>
+
+              <v-btn icon class="px-8">
+                <v-icon v-on:click= "github">mdi-github-circle</v-icon>
+              </v-btn>
+
+              <v-btn icon class="px-8">
+                <v-icon v-on:click = "linkedin">mdi-linkedin-box</v-icon>
+              </v-btn>
+              
+            </div>
+
+        </v-card-title>
+
+        <v-card-text class="py-2 white--text text-center orange lighten-1">
+
+          {{ new Date().getFullYear() }} — <strong>Eddy's Portfolio</strong>
+
+        </v-card-text>
+
+      </v-card>
+
+  </v-footer>
+
+</template>
+
+<script>
+export default {
+    name: 'footer',
+
+    methods: {
+      twitter: function () {
+      window.location.href = "https://twitter.com/SandwichPirate_"
+      },
+      github: function() {
+        window.location.href = "https://github.com/EddyScript"
+      },
+      linkedin: function() {
+        window.location.href = "https://www.linkedin.com/in/eddy-balan-270132175/"
+      }
+    }
+}
+</script>
+
+<style>
+
+  #title {
+    font-family: 'Raleway', sans-serif;
+    font-size: 26px;
+  }
+
+</style>
